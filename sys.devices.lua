09928948f34db420a99050c6d8760c339c9ce98a
Artem Rudnitsky <artem.rudnitsky@gmail.com>
2019-05-30 11:03:01 +0000

    [fix] Разбор свойств.

--- sys.devices.lua
+++ sys.devices.lua
@@ -38,15 +38,18 @@ system.init_devices_properties = function( self )
             if device ~= nil then
 
                 for field, value in pairs( device_descr.prop ) do
-                    if value ~= '' then
-                        assert( loadstring( "dev = __"..value ) )( )
-                        if dev == nil then
-                            error( "Unknown device '"..
-                                value.."' (__"..value..")." )
-                        end
-
-                        device:set_property( field, dev )
-                    end
+                	if field == MT then
+	                    if value ~= '' then
+	                        assert( loadstring( "dev = __"..value ) )( )
+	                        if dev == nil then
+	                            error( "Unknown device '"..
+	                                value.."' (__"..value..")." )
+	                        end
+	                        device:set_property( field, dev )
+	                    end
+	                else
+	                	device:set_string_property( field, value )
+	                end
                 end
             end
         end --if
