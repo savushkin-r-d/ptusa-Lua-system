d3301e7687516b72b9bdfa00f68ea0d4f27f226b
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2021-04-20 16:54:12 +0300

    Change devices' parameters reading

--- sys.devices.lua
+++ sys.devices.lua
@@ -3,108 +3,82 @@
 system = system or {}
 
 -- ----------------------------------------------------------------------------
---Инициализация имен устройств.
+--Инициализация описания устройств.
 system.init_devices_names = function( self )
-
     local devices_count = #devices
-
     for i = 1, devices_count do
+        local descr = devices[ i ]
+        local device = G_DEVICE_MANAGER():get_device( descr.dtype, descr.name )
 
-        local device = G_DEVICE_MANAGER():get_device(
-            devices[ i ].dtype, devices[ i ].name )
-
-        if device ~= nil then
-            device:set_descr( devices[ i ].descr or "" )
-        end --if
-
-    end --for i = 1, devices_count do
+        if device then
+            device:set_descr( descr.descr or "" )
+        end
+    end
 end
 -- ----------------------------------------------------------------------------
 --Инициализация свойств устройств.
 system.init_devices_properties = function( self )
-
     local devices_count = #devices
-
     for i = 1, devices_count do
-
-        local device_descr = devices[ i ]
+        local descr = devices[ i ]
 
         --Есть дополнительные свойства.
-        if device_descr.prop ~= nil then
+        if descr.prop then
+            local device = G_DEVICE_MANAGER():get_device( descr.dtype, descr.name )
 
-            local device = G_DEVICE_MANAGER():get_device(
-                devices[ i ].dtype, devices[ i ].name )
-
-            if device ~= nil then
-
-                for field, value in pairs( device_descr.prop ) do
-                	if field == MT then
-	                    if value ~= '' then
-	                        assert( loadstring( "dev = __"..value ) )( )
-	                        if dev == nil then
-	                            error( "Unknown device '"..
-	                                value.."' (__"..value..")." )
-	                        end
-	                        device:set_property( field, dev )
-	                    end
-	                else
-	                	device:set_string_property( field, value )
-	                end
+            if device then
+                for field, value in pairs( descr.prop ) do
+                    if field == "MT" then
+                        if value ~= '' then
+                            assert( loadstring( "dev = __"..value ) )( )
+                            if not dev then
+                                error( "init_devices_properties() - unknown device '"..value.."'." )
+                            end
+                            device:set_property( field, dev )
+                        end
+                    else
+                        device:set_string_property( field, value )
+                    end
                 end
             end
-        end --if
-    end --for i = 1, devices_count do
+        end
+    end
 end
 -- ----------------------------------------------------------------------------
 --Инициализация параметров устройств.
 system.init_devices_params = function( self )
-
     local devices_count = #devices
-
     for i = 1, devices_count do
+        local descr = devices[ i ]
+        local device = G_DEVICE_MANAGER():get_device( descr.dtype, descr.name )
 
-        local device_descr = devices[ i ]
-
-        local device = G_DEVICE_MANAGER():get_device(
-            devices[ i ].dtype, devices[ i ].name )
-
-        if device ~= nil then
-
-            local par_count = 0
-            if device_descr.par ~= nil then
-                par_count = #device_descr.par
+        if device and descr.par then
+            local params_count = #descr.par
+            for j = 1, params_count do
+                device:set_par( j, 0, descr.par[ j ] )
             end
 
-            for j = 1, par_count do
-                device:set_par( j, 0, device_descr.par[ j ] )
+            for key, value in pairs( descr.par ) do
+                if type( key ) == "string" then
+                    device:set_cmd( key, 0, value )
+                end
             end
-
-        end --if
-    end --for i = 1, devices_count do
+        end
+    end
 end
 -- ----------------------------------------------------------------------------
 --Инициализация рабочих параметров устройств.
 system.init_devices_rt_params = function( self )
-
     local devices_count = #devices
-
     for i = 1, devices_count do
+        local descr = devices[ i ]
+        local device = G_DEVICE_MANAGER():get_device( descr.dtype, descr.name )
 
-        local device_descr = devices[ i ]
-
-        local device = G_DEVICE_MANAGER():get_device(
-            devices[ i ].dtype, devices[ i ].name )
-
-        if device ~= nil then
-            local rt_par_count = 0
-            if device_descr.rt_par ~= nil then
-                rt_par_count = #device_descr.rt_par
-            end
-
+        if device and descr.rt_par then
+            local rt_par_count = #descr.rt_par
             for j = 1, rt_par_count do
-                device:set_rt_par( j, device_descr.rt_par[ j ] )
+                device:set_rt_par( j, descr.rt_par[ j ] )
             end
-
-        end --if
-    end --for i = 1, devices_count do
+        end
+    end
 end
