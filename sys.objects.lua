4c9e59ff7743098a33255b626fe25966a6e2c8bf
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-05-24 17:55:18 +0300

    Add delay to switched on devices (#7)
    * Add delay to switched on devices
    
    * Refactor algorithm
    
    * Remove previous devices processing
    
    * Use separate action

--- sys.objects.lua
+++ sys.objects.lua
@@ -417,6 +417,20 @@ init_tech_objects = function()
             local step_w = mode[ state_n ][ step_n ][ step.A_WASH ]
             proc_devices_action( value.wash_data, 1, step_w )
         end
+
+        if value.delay_opened_devices then
+            for sub_group, group in pairs( value.delay_opened_devices ) do
+                process_dev_ex( mode, state_n, step_n, step.A_DELAY_ON, group[ 1 ],
+                    0, sub_group - 1 )
+
+                --Добавляем индекс параметра производительности.
+                local param_idx = group[ 2 ]
+                if param_idx then
+                    local a_step = mode[ state_n ][ step_n ][ step.A_DELAY_ON ]
+                    a_step:set_param_idx( sub_group - 1, param_idx )
+                end
+            end
+        end
     end
 
     --Пример команды от сервера в виде скрипта:
